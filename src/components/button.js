2698af4647149e4f55a867e515e91a21969b47f4
Rajeev Krishna <lambo2572@gmail.com>
2023-06-25 00:07:12 +0530

    Minor changes in the button

--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,7 @@
 export const Button = () => {
     return(
         <div className="btn mx-auto my-5">
-            <button className="p-2 text-green-300 border-2 hover:rounded-md hover:scale-105 hover:font-bold">Click Me</button>
+            <button className="p-2 text-green-300 border rounded-md hover:scale-105 hover:font-bold hover:bg-red-500 hover:text-white" onClick={() => alert("Alert")}>Alert ⚠️</button>
         </div>
     )
 }
